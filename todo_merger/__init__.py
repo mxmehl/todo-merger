e2ef553db52b64a95468bcaee4628d8d391343c3
Max Mehl <mail@mehl.mx>
2024-07-26 09:19:54 +0200

    daemonize app

--- todo_merger/__init__.py
+++ todo_merger/__init__.py
@@ -1,33 +1,80 @@
-"""App Init"""
+"""ToDo Merger: Provide an overview of your assigned issues on GitLab and GitLab"""
 
 import argparse
 import logging
+import signal
 import sys
 from importlib.metadata import version
+from os import kill, path
 
-import setuptools
+import daemon
+import daemon.pidfile
 from flask import Flask
 from github import Github
 from gitlab import Gitlab
+from platformdirs import user_log_dir, user_runtime_dir
 from sassutils.wsgi import SassMiddleware
 
 from ._auth import github_login, gitlab_login
-from ._config import get_app_config
+from ._config import default_config_file_path, get_app_config
+
+LOGFILE = path.join(user_log_dir("todo-merger", ensure_exists=True), "todo-merger.log")
+PIDFILE = path.join(user_runtime_dir("todo-merger", ensure_exists=True), "todo-merger.pid")
 
 __version__ = version("todo-merger")
 
 parser = argparse.ArgumentParser(
     description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
+subparsers = parser.add_subparsers(dest="command", help="Special commands")
+
 parser.add_argument(
-    "-c",
-    "--config-file",
-    help="Path to the app config file in a non-default location",
+    "-c", "--config-file", help="Path to the app config file ", default=default_config_file_path()
+)
+parser.add_argument(
+    "-d",
+    "--daemon",
+    help="Start the app in the background and log to a file",
+    action="store_true",
+)
+parser.add_argument(
+    "-l", "--logfile", help="Path to the log file (in daemon mode)", default=LOGFILE
+)
+parser.add_argument(
+    "-p", "--pidfile", help="Path to the PID file (in daemon mode)", default=PIDFILE
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    help="Activate INFO logging",
+    action="store_true",
+)
+parser.add_argument(
+    "-vv",
+    "--debug",
+    help="Activate DEBUG logging (even more than INFO)",
+    action="store_true",
 )
 parser.add_argument("--version", action="version", version="%(prog)s " + __version__)
 
+# Special commands
+parser_stop = subparsers.add_parser(
+    "stop",
+    help="Stop the running todo-merger background instance",
+)
 
-logging.basicConfig(level=logging.DEBUG)
+
+def configure_logger(args) -> logging.Logger:
+    """Set logging options"""
+    log = logging.getLogger()
+    logging.basicConfig(
+        encoding="utf-8",
+        format="%(levelname)s in %(module)s: %(message)s",
+        level=(logging.DEBUG if args.debug else logging.INFO if args.verbose else logging.WARNING),
+        handlers=[logging.StreamHandler()],
+    )
+
+    return log
 
 
 def load_app_config(config_file: str) -> dict[str, tuple[str, Github | Gitlab]]:
@@ -74,12 +121,18 @@ def load_app_config(config_file: str) -> dict[str, tuple[str, Github | Gitlab]]:
 # pylint: disable=import-outside-toplevel
 def create_app(config_file: str):
     """Create Flask App"""
+
+    # Initiate Flask app
     app = Flask(__name__)
     # Reload templates
     app.jinja_env.auto_reload = True
     app.jinja_env.lstrip_blocks = False
     app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+    # Set werkzeug logging level to the global logging level
+    logging.getLogger("werkzeug").setLevel(logging.root.level)
+
+    # Settings for SCSS conversion
     app.wsgi_app = SassMiddleware(  # type: ignore
         app.wsgi_app,
         {
@@ -105,8 +158,41 @@ def create_app(config_file: str):
     return app
 
 
+def run_server(config_file: str):
+    """Run the Flask server"""
+    app = create_app(config_file=config_file)
+    app.run(port=8636)
+
+
 def main():
     """Main entry point for running the app."""
     args = parser.parse_args()
-    app = create_app(config_file=args.config_file)
-    app.run(port=8636)
+
+    # Configure logger
+    logger = configure_logger(args=args)
+
+    # Stop app if requested
+    if args.command == "stop":
+        try:
+            with open(args.pidfile, "r", encoding="utf-8") as pidreader:
+                pid = int(pidreader.read())
+                print(f"Sending SIGTERM to process {pid}")
+                kill(pid, signal.SIGTERM)
+                sys.exit()
+        except FileNotFoundError:
+            sys.exit(
+                f"PID file {args.pidfile} does not seem to exist. The app does not seem to run "
+                "normally or at all. Check your task manager and process list."
+            )
+
+    # Start app
+    print("ToDo Merger will be available on http://localhost:8636")
+    if args.daemon:
+        with daemon.DaemonContext(pidfile=daemon.pidfile.TimeoutPIDLockFile(args.pidfile)):
+            # Add file logger
+            logger.addHandler(logging.FileHandler(args.logfile))
+
+            # Run server
+            run_server(config_file=args.config_file)
+    else:
+        run_server(config_file=args.config_file)
